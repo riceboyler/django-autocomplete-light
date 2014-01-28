532dfab7cf04b7605a089f5da51181706027c5e9
jpic <jamespic@gmail.com>
2014-01-28 16:06:32 +0100

    Cosmetics

--- autocomplete_light/example_apps/readonly_autocomplete_field/admin.py
+++ autocomplete_light/example_apps/readonly_autocomplete_field/admin.py
@@ -9,21 +9,17 @@ from . import forms
 admin.site.register(RelatedModel)
 
 
-class ReadonlyFieldsMixin(admin.ModelAdmin):
+class ReadonlyFieldsModelAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, request, obj=None):
         return ['relation'] if obj is not None and obj.pk else []
 
     def get_form(self, request, obj=None, **kwargs):
         kwargs['formfield_callback'] = autocomplete_light.FormfieldCallback()
-        return super(ReadonlyFieldsMixin, self).get_form(request, obj, **kwargs)
+        return super(ReadonlyFieldsModelAdmin, self).get_form(request, obj, **kwargs)
 
 
 for model in (TestFk, TestMtm, TestOto):
-    attrs = {
-        'readonly_fields': ['relation'],
-    }
-
     ModelAdmin = type('%sModelAdmin' % model.__name__,
-                      (ReadonlyFieldsMixin,), attrs)
+                      (ReadonlyFieldsModelAdmin,), attrs)
 
     admin.site.register(model, ModelAdmin)
